e298cd0ff35ba4d2eddedf17d7b28ec8c1eea15c
jide8 <jideoketayo08@gmail.com>
2024-06-27 09:50:54 +0100

    finished original

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import Nav from "./sections/Nav";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
@@ -7,7 +7,7 @@ import Reviews from "./sections/Reviews";
 import Footer from "./sections/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
